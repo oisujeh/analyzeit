0a4eeb8667d3929a165e2ad1b7ab2c8472c1f0f6
oisujeh <33390655+oisujeh@users.noreply.github.com>
2023-02-09 16:56:22 +0100

    09022023_04:56

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -12,12 +12,13 @@ class ReportController extends Controller
 {
     public function vlcascade(): Factory|View|Application
     {
-        $data = DB::table('gen_vl_cascade')
+        $vlCascade = DB::table('gen_vl_cascade')
             ->select('state as state','active as active','eligible as eligible','samp_collected as samp_collected',
                 'no_vl_result as vl_result','percent_vl_result as percent_result','suppressed as suppressed','percent_suppressed
                  as percent_suppressed','gap as gap','result_lt_50 as result_lt_50','result_50_999 as result_50_999',
                 'result_gt_999 as result_gt_999')
+            ->orderBy('state','asc')
             ->get();
-        return view('reports.vl_cascade',compact('data'));
+        return view('reports.vl_cascade',compact('vlCascade'));
     }
 }
--- resources/views/reports/vl_cascade.blade.php
+++ resources/views/reports/vl_cascade.blade.php
@@ -1,14 +1,60 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('VL Cascade') }}
+            {{ __('Viral Load Cascade by State') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class=" overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="grid sm:grid-cols-1 bg-white drop-shadow-md mt-4 text-md">
+                <div class="container mx-auto">
+                    <div class="flex flex-col">
+                        <div class="w-full">
+                            <div class="p-4 border-b border-gray-200 shadow">
+                                <!-- <table> -->
+                                <table id="emr-table" class="p-4 max-w-full w-full mx-auto myTables">
 
+                                    <thead>
+                                    <tr class="text-md font-semibold text-center border-b-2 border-blue-500">
+                                        <th class="border">State</th>
+                                        <th class="border">Tx_Curr</th>
+                                        <th class="border">Eligible</th>
+                                        <th class="border">Sample Collected</th>
+                                        <th class="border">Eligible with Result</th>
+                                        <th class="border">Coverage</th>
+                                        <th class="border">Suppressed</th>
+                                        <th class="border">% Suppressed</th>
+                                        <th class="border text-red-600">GAP</th>
+                                        <th class="border bg-indigo-100">VL < 50</th>
+                                        <th class="border bg-indigo-100">VL 50 - 999</th>
+                                        <th class="border bg-indigo-100">Unsuppressed</th>
+                                    </tr>
+                                    </thead>
+
+                                    <tbody>
+                                    @foreach($vlCascade as $list)
+                                        <tr class="hover:bg-gray-200 font-medium">
+                                            <td class="px-4 py-2 border">{{$list->state}}</td>
+                                            <td class="px-4 py-2 border">{{number_format($list->active) }}</td>
+                                            <td class="px-4 py-2 border">{{number_format($list->eligible)}}</td>
+                                            <td class="px-4 py-2 border">{{number_format($list->samp_collected)}}</td>
+                                            <td class="px-4 py-2 border">{{number_format($list->vl_result)}}</td>
+                                            <td class="px-4 py-2 border">{{$list->percent_result}}</td>
+                                            <td class="px-4 py-2 border">{{number_format($list->suppressed)}}</td>
+                                            <td class="px-4 py-2 border">{{$list->percent_suppressed}}</td>
+                                            <td class="px-4 py-2 border">{{number_format($list->gap)}}</td>
+                                            <td class="px-4 py-2 border bg-indigo-100">{{number_format($list->result_lt_50)}}</td>
+                                            <td class="px-4 py-2 border bg-indigo-100">{{number_format($list->result_50_999)}}</td>
+                                            <td class="px-4 py-2 border bg-indigo-100">{{number_format($list->result_gt_999)}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
