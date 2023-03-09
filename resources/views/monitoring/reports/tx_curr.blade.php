e97d236a04569ffa0b808b44ad1281b9e35aed14
oisujeh <33390655+oisujeh@users.noreply.github.com>
2023-03-09 16:44:47 +0100

    Updated to laravel 10

--- resources/views/monitoring/reports/tx_curr.blade.php
+++ resources/views/monitoring/reports/tx_curr.blade.php
@@ -46,8 +46,7 @@
                     <ul class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden
                         rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg
                         dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-                        aria-labelledby="dropdownMenuButton2"
-                        data-te-dropdown-menu-ref>
+                        aria-labelledby="dropdownMenuButton2" data-te-dropdown-menu-ref>
                         <li>
                             <a class="block w-full whitespace-nowrap py-2 px-4 text-sm font-normal bg-white text-neutral-700
                                 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none
